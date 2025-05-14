87ada835142b6d6f4fe158b14aa721da1f10c2dd
Antsurf <antoine.richard972@gmail.com>
2025-05-14 12:15:52 +0200

    Add animation when displaying table see if we can center the view of user when we open table

--- JS/home.js
+++ JS/home.js
@@ -1,12 +1,20 @@
-// generation of the multiplication tables
-
 const container = document.getElementById('tables-container');
 
 for (let i = 1; i <= 12; i++) {
     const table = document.createElement('table');
     table.id = `tablemult-${i}`;
+    table.className = 'tables-content';
+
+    // Add hidden class to table initially, we know have two classes
+    table.classList.add('hidden-table');
+
     const caption = document.createElement('caption');
     caption.textContent = `Table of ${i}`;
+    // Make caption clickable
+    caption.style.cursor = 'pointer'; // change cursor into a hand
+    caption.addEventListener('click', function() {
+        table.classList.toggle('hidden-table');
+    });
     table.appendChild(caption);
 
     for (let j = 1; j <= 12; j++) {
@@ -19,5 +27,3 @@ for (let i = 1; i <= 12; i++) {
 
     container.appendChild(table);
 }
-
-// generation of the division tables
